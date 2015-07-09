f93dc30c6dbc5f7c0acf5f2f95be82c4e2a398aa
ysc3839 <ysc3839@163.com>
2015-07-09 14:40:40 +0800

    完善代码 添加VS解决方案

--- dll.cpp
+++ dll.cpp
@@ -1,39 +1,40 @@
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <stdio.h>
 
-BOOL APIENTRY DllMain (HINSTANCE hInst     /* Library instance handle. */ ,
-                       DWORD reason        /* Reason this function is being called. */ ,
-                       LPVOID reserved     /* Not used. */ )
+BOOL APIENTRY DllMain(HINSTANCE hInst     /* Library instance handle. */,
+	DWORD reason        /* Reason this function is being called. */,
+	LPVOID reserved     /* Not used. */)
 {
 	switch (reason)
-    {
-      case DLL_PROCESS_ATTACH:
-           MessageBox (0, "From DLL\n", "Process Attach", MB_ICONINFORMATION);
-        break;
+	{
+	case DLL_PROCESS_ATTACH:
+		MessageBox(0, TEXT("From DLL\n"), TEXT("Process Attach"), MB_ICONINFORMATION);
+		break;
 
-      case DLL_PROCESS_DETACH:
-           MessageBox (0, "From DLL\n", "Process Detach", MB_ICONINFORMATION);
-        break;
+	case DLL_PROCESS_DETACH:
+		MessageBox(0, TEXT("From DLL\n"), TEXT("Process Detach"), MB_ICONINFORMATION);
+		break;
 
-      case DLL_THREAD_ATTACH:
-           MessageBox (0, "From DLL\n", "Thread Attach", MB_ICONINFORMATION);
-        break;
+	case DLL_THREAD_ATTACH:
+		MessageBox(0, TEXT("From DLL\n"), TEXT("Thread Attach"), MB_ICONINFORMATION);
+		break;
 
-      case DLL_THREAD_DETACH:
-           MessageBox (0, "From DLL\n", "Thread Detach", MB_ICONINFORMATION);
-        break;
-    }  
+	case DLL_THREAD_DETACH:
+		MessageBox(0, TEXT("From DLL\n"), TEXT("Thread Detach"), MB_ICONINFORMATION);
+		break;
+	}
 
-    return TRUE;
+	return TRUE;
 }
 
 
-int WINAPI WinMain(      
-    HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine,
-    int nCmdShow)
-{  
-  MessageBox (0, "From DLL\n", "Process Attach", MB_ICONINFORMATION);
-  return 0;
-}
+/*int WINAPI WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	MessageBox(0, "From DLL\n", "Process Attach", MB_ICONINFORMATION);
+	return 0;
+}*/
